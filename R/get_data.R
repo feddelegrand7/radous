f40a55d22560e7cc426dac5800f45466ce5bd754
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2020-10-29 14:02:33 +0100

    func documented, description done, license added

--- R/get_data.R
+++ R/get_data.R
@@ -1,11 +1,24 @@
 
 
+#' Query random user data from the Random User Generator API
+#'
+#' @param n Number of users to fetch, up to 5,000 generated users in one request. Defaults to 1
+#' @param seed A character string used to generate the same set of users. Defaults to NULL.
+#'
+#' @return A tibble that contains random user data 
+#' @export
+#'
+#' @examples
+#' 
+#' get_data(n = 10, seed = "1990")
+#'
+
 get_data <- function(n = 1, seed = NULL) {
 
 
-link <- ifelse(is.null(seed), 
+link <- ifelse(is.null(seed),
 
-glue::glue("https://randomuser.me/api/?results={n}&format=csv"), 
+glue::glue("https://randomuser.me/api/?results={n}&format=csv"),
 
 glue::glue("https://randomuser.me/api/?seed={seed}&results={n}&format=csv")
 
