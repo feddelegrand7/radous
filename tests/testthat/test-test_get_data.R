31847d484f0768fa5cefbf34717fc93b1b879701
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-01-18 22:02:13 +0100

    TESTS

--- tests/testthat/test-test_get_data.R
+++ tests/testthat/test-test_get_data.R
@@ -2,23 +2,26 @@
 
 data <- httr::RETRY(
   "GET",
-  "https://randomuser.me/api/?seed=123&results=100&format=csv",
-  times = 3)
+  "https://randomuser.me/api/?seed=123&results=10&format=csv",
+  times = 5)
 
 
 text <- httr::content(data, as = "text")
 
 data <- readr::read_csv(text)
 
-final <- janitor::clean_names(data)
+names(data) <- gsub(".", "_", names(data), fixed = TRUE)
+
+final <- data
 
 
 test_that("testing identical results", {
 
+  testthat::expect_identical(final,get_data(n = 10, seed = "123"))
 
-  testthat::expect_s3_class(get_data(n = 10), "data.frame")
+  testthat::expect_s3_class(final, "data.frame")
 
-  testthat::expect_equal(ncol(final), ncol(get_data(n = 10, seed = "123")))
+  testthat::expect_equal(ncol(final), ncol(get_data(n = 10)))
 
 
 })
